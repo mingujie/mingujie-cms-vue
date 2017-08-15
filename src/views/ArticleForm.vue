a6920d75eafe3fe8282b39ec0b099f787b3db664
左继权 <zuopro@MacBook-Pro.local>
2017-08-15 18:12:14 +0800

    文章表单页面

--- src/views/ArticleForm.vue
+++ src/views/ArticleForm.vue
@@ -9,13 +9,29 @@
           <div class="form-group row">
             <label for="text-input" class="col-md-1 form-control-label">标题</label> 
             <div class="col-md-11">
-                <el-input placeholder="标题最多不超过60个字符" v-model="input4">
+                <el-input placeholder="标题最多不超过60个字符" v-model="form.title">
                   <template slot="append"><span>0</span>/60</template>
                 </el-input>
             </div>
           </div>
-            
-              
+          <div class="form-group row">
+            <label for="text-input" class="col-md-1 form-control-label">副标题</label> 
+            <div class="col-md-11">
+                <el-input placeholder="标题最多不超过60个字符" v-model="form.subTitle">
+                  <template slot="append"><span>0</span>/60</template>
+                </el-input>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="text-input" class="col-md-1 form-control-label">封面</label> 
+            <div class="col-md-11">
+            <el-radio disabled v-model="form.cover" label="禁用">备选项</el-radio>
+            <el-radio disabled v-model="form.cover" label="选中且禁用">备选项</el-radio></div>
+          </div>
+          <div class="form-group row">
+            <label for="text-input" class="col-md-1 form-control-label">摘要</label> 
+            <div class="col-md-11">
+            <el-input type="textarea" v-model="form.desc"></el-input>          </div>
             </el-form>
           </div>
         </div>
@@ -29,23 +45,16 @@ export default {
   name: 'dashboard',
   data () {
     return {
-      keyword: '',
-      currentPage: 1,
       form: {
-        name: ''
+        title: '',
+        subTitle: '',
+        name: '',
+        cover: '',
+        desc: ''
       }
     }
   },
   methods: {
-    searchArticle () {
-
-    },
-    handleSizeChange (size) {
-
-    },
-    handleCurrentChange (currentPage) {
-      console.log(currentPage)
-    }
   }
 }
 </script>
