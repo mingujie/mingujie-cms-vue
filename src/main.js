8b3176a624a73e4193b443dda33fb434cd66776a
左继权 <zuopro@MacBook-Pro.local>
2017-08-15 21:56:43 +0800

    增加上传图片功能

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Input, Button, Pagination, Form, Radio} from 'element-ui'
+import {Input, Button, Pagination, Form, Radio, Dialog} from 'element-ui'
 Vue.component(Input.name, Input)
 Vue.component(Pagination.name, Pagination)
 Vue.component(Button.name, Button)
 Vue.component(Form.name, Form)
 Vue.component(Radio.name, Radio)
+Vue.component(Dialog.name, Dialog)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/views/ArticleForm.vue
+++ src/views/ArticleForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="animated fadeIn">
+  <div class="animated">
     <div class="row">
       <div class="col-sm-12 col-md-12">
         <div class="card">
@@ -26,7 +26,7 @@
               </div>
             </div>
             <div class="form-group row">
-              <label for="text-input" class="col-md-1 form-control-label">富文本</label> 
+              <label for="text-input" class="col-md-1 form-control-label">正文</label> 
               <div class="col-md-11">
               <quill-editor :rows="8" ref="myTextEditor"
                             v-model="editor.content"
@@ -40,13 +40,14 @@
             <div class="form-group row">
               <label for="text-input" class="col-md-1 form-control-label">封面</label> 
               <div class="col-md-11">
-                <el-radio class="radio" v-model="form.cover" label="1">单图</el-radio>
+                <el-button size="small">从正文选择</el-button>
+                <el-button size="small" @click="dialogPhotoVisible = true">从图片库选择</el-button>
               </div>
             </div>
             <div class="form-group row">
               <label for="text-input" class="col-md-1 form-control-label">摘要</label> 
               <div class="col-md-11">
-                <label for="prependedInput" class="form-control-label">Prepended text</label>
+                <label for="prependedInput" class="form-control-label">选填，如果不填写会默认抓取正文前54个字</label>
                 <el-input type="textarea" :rows="4" v-model="form.desc"></el-input>
               </div>
             </div>
@@ -62,6 +63,40 @@
         </div>
       </div><!--/.col-->
     </div>
+    <el-dialog
+      title="选择图片"
+      :visible.sync="dialogPhotoVisible"
+      size="small"
+      :before-close="handleClose" class="dialogPhoto">
+        <div class="card">
+          <div class="card-header"> 
+           <el-button type="success" size="small">本地上传</el-button>    
+          </div>
+          <div class="card-block">
+            <div class="row">
+              <div class="col-md-3">
+1
+              </div>
+                            <div class="col-md-3">
+2
+              </div>
+
+              <div class="col-md-3">
+3
+              </div>
+              <div class="col-md-3">
+4
+              </div>
+
+            </div>
+          </div>
+      </div>
+       <span slot="footer" class="dialog-footer">
+
+        <el-button @click="dialogPhotoVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogPhotoVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -72,6 +107,7 @@ export default {
   components: { quillEditor },
   data () {
     return {
+      dialogPhotoVisible: false,
       editor: {
         options: {
 
@@ -88,6 +124,12 @@ export default {
     }
   },
   methods: {
+    handleClose (done) {
+      done()
+    },
+    onRadioChange (val) {
+      console.log(this.form)
+    },
     onEditorBlur () {
 
     },
@@ -100,3 +142,11 @@ export default {
   }
 }
 </script>
+<style>
+  .dialogPhoto .el-dialog {
+    width : 845px !important;
+  }
+.ql-container.ql-snow {
+  height: 360px !important;
+}
+</style>
