e85954e1c31372ab2b3c9e0b3211d160eb6e1b79
pifupai <mingu@pifupai.com>
2017-08-15 23:32:09 +0800

    修改侧边栏

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="sidebar">
-    <nav class="sidebar-nav">
+    <el-menu default-active="2" theme="dark" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-document"></i>内容管理</template>
+        <el-menu-item-group>
+          <template slot="title">PC系列</template>
+          <el-menu-item index="1-1">英雄联盟</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group>
+          <template slot="title">手游系列</template>
+          <el-submenu index="1-4">
+            <template slot="title">王者荣耀</template>
+            <el-menu-item index="1-4-2">新闻</el-menu-item>
+            <el-menu-item index="1-4-3">皮肤</el-menu-item>
+            <el-menu-item index="1-4-4">视频</el-menu-item>
+            <el-menu-item index="1-4-5">图片</el-menu-item>
+          </el-submenu>
+        </el-menu-item-group>
+
+      </el-submenu>
+      <el-menu-item index="2"><i class="el-icon-picture"></i>图片管理</el-menu-item>
+      <el-menu-item index="3"><i class="el-icon-setting"></i>会员管理</el-menu-item>
+      <el-menu-item index="3"><i class="el-icon-setting"></i>系统设置</el-menu-item>
+    </el-menu>
+<!--     <nav class="sidebar-nav">
       <ul class="nav">
         
         <li class="nav-item">
@@ -27,7 +50,7 @@
           <router-link :to="'#'" class="nav-link"><i class="icon-speedometer"></i> 系统设置 </router-link>
         </li>
       </ul>
-    </nav>
+    </nav> -->
   </div>
 </template>
 <script>
@@ -38,6 +61,12 @@ export default {
     handleClick (e) {
       e.preventDefault()
       e.target.parentElement.classList.toggle('open')
+    },
+    handleOpen () {
+
+    },
+    handleClose () {
+
     }
   }
 }
@@ -46,5 +75,8 @@ export default {
 <style lang="css">
   .nav-link {
     cursor:pointer;
+  }
+  .el-menu--dark {
+      background-color: #263238;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Input, Button, Pagination, Form, Radio, Dialog} from 'element-ui'
+import {Input, Button, Pagination, Form, Radio, Dialog, Menu, Submenu, MenuItem, MenuItemGroup} from 'element-ui'
 Vue.component(Input.name, Input)
 Vue.component(Pagination.name, Pagination)
 Vue.component(Button.name, Button)
 Vue.component(Form.name, Form)
 Vue.component(Radio.name, Radio)
 Vue.component(Dialog.name, Dialog)
+Vue.component(Menu.name, Menu)
+Vue.component(Submenu.name, Submenu)
+Vue.component(MenuItem.name, MenuItem)
+Vue.component(MenuItemGroup.name, MenuItemGroup)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
